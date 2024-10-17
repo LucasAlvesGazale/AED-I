6169229eb7a80f970eb57e28fcf696ad6c8c0a98
LucasAlvesGazale <lucasgazale@gmail.com>
2024-10-17 10:34:35 -0300

    Create 1002.c

---
+++ 1002.c
@@ -0,0 +1,16 @@
+/*
+Exercício 1000
+Autor: Lucas Alves Gazale
+RA: 176565
+Data: 13/10/2024
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double Raio;
+    scanf("%lf", &Raio);
+    printf("A=%.4f\n", Raio*Raio*3.14159);
+}
