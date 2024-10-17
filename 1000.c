727c59a14b52ff1d6bd7b3a32e52963b0f0a6edf
LucasAlvesGazale <lucasgazale@gmail.com>
2024-10-17 10:31:50 -0300

    Create 1000.c

---
+++ 1000.c
@@ -0,0 +1,16 @@
+/*
+Exercício 1000
+Autor: Lucas Alves Gazale
+RA: 176565
+Data: 13/10/2024
+
+Este programa imprime "Hello, World!" na tela
+*/ 
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
