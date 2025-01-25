4b18a02163c5b292d1359c8ff45afab24d51bfa3
LucasAlvesGazale <lucasgazale@gmail.com>
2025-01-25 19:49:43 -0300

    Create 1036.c

---
+++ 1036.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double A, B, C;
+    scanf("%lf %lf %lf", &A, &B, &C);
+    
+    // Verifica se A é zero ou se o discriminante é negativo
+    if (A == 0 || (B * B - 4 * A * C) < 0) {
+        printf("Impossivel calcular\n");
+    } else {
+        double delta = B * B - 4 * A * C;
+        double R1 = (-B + sqrt(delta)) / (2 * A);
+        double R2 = (-B - sqrt(delta)) / (2 * A);
+        
+        printf("R1 = %.5lf\n", R1);
+        printf("R2 = %.5lf\n", R2);
+    }
+    
+    return 0;
+}
