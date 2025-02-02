0cd7c666116fa43f3e7636127872e8c43bb586e1
LucasAlvesGazale <lucasgazale@gmail.com>
2025-02-02 18:08:19 -0300

    Update 1035.c

--- 1035.c
+++ 1035.c
@@ -3,10 +3,8 @@
 int main() {
     int A, B, C, D;
     
-    // Leitura dos quatro valores inteiros
     scanf("%d %d %d %d", &A, &B, &C, &D);
     
-    // Verificação das condições
     if (B > C && D > A && (C + D) > (A + B) && C > 0 && D > 0 && A % 2 == 0) {
         printf("Valores aceitos\n");
     } else {
