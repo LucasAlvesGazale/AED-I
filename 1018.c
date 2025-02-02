090bb70efa9489232ed2c4bf323c409cd639b3cf
LucasAlvesGazale <lucasgazale@gmail.com>
2025-02-02 18:06:04 -0300

    Update 1018.c

--- 1018.c
+++ 1018.c
@@ -3,21 +3,16 @@
 int main() {
     int N;
     
-    // Leitura do valor inteiro
     scanf("%d", &N);
     
-    // Exibindo o valor inicial
     printf("%d\n", N);
     
-    // Definindo os valores das notas
     int notas[] = {100, 50, 20, 10, 5, 2, 1};
-    
-    // Para cada tipo de nota, calculamos a quantidade de notas
+
     for (int i = 0; i < 7; i++) {
-        int quantidade = N / notas[i];  // Calcula a quantidade de notas de valor notas[i]
-        N %= notas[i];  // Atualiza N para o restante após a divisão
+        int quantidade = N / notas[i];  
+        N %= notas[i];  
         
-        // Exibe o resultado no formato solicitado
         printf("%d nota(s) de R$ %d,00\n", quantidade, notas[i]);
     }
     
